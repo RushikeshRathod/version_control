a8e1759e6c293abb98160763ccce4f5af9def614
rushikesh <rushikeshbrathod@gmail.com>
2020-06-17 19:54:41 +0530

    dog can now bark

--- animal.py
+++ animal.py
@@ -1,6 +1,12 @@
+import sys
 def default():
     print('hello')
+def dog():
+    print('dog is barking, bho! bho! get afraid')
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
 if __name__ =='__main__':
     main()
